90d7b74d681b3164b9f00ae95a286d0046b28127
zhanghanduo <hzhang032@e.ntu.edu.sg>
2017-06-08 12:30:16 +0800

    add listener2

--- listener2.cpp
+++ listener2.cpp
@@ -12,7 +12,7 @@
 #include <iostream>
 #include <string>
 #include <lcm/lcm-cpp.hpp>
-#include "../obset_t.hpp"
+#include "exlcm/example_t.hpp"
 using namespace std;
 
 class Handler
@@ -22,26 +22,26 @@ public:
 
     void handleMessage(const lcm::ReceiveBuffer* rbuf,
                        const std::string& chan,
-                       const exlcm::obset_t* msg)
+                       const exlcm::example_t* msg)
     {
         int i;
-        std::vector <exlcm::obs_t> receive;
-        receive.resize(msg->obstacle_set.size());
-        receive = msg->obstacle_set;
+//        std::vector <exlcm::example_t> receive;
+//        receive.resize(msg->obstacle_set.size());
+//        receive = msg->obstacle_set;
         printf("Received message on channel \"%s\":\n", chan.c_str());
         cout << "type of the message: " << msg->name.c_str() << endl;
-        printf("  timestamp   = %f\n", msg->utime);
+        printf("  timestamp   = %ld\n", msg->timestamp);
         printf("  position    = (%f, %f, %f)\n",
                msg->position[0], msg->position[1], msg->position[2]);
         printf("  orientation = (%f, %f, %f, %f)\n",
                msg->orientation[0], msg->orientation[1],
                msg->orientation[2], msg->orientation[3]);
-        cout << "num of obstacles: " << msg->num << endl;
-        for(i = 0; i < msg->num; i++)
-            printf("max disparity of %d is: %d", i, receive[i].max_disparity);
+        cout << "num of ranges: " << msg->num_ranges << endl;
+        for(i = 0; i < msg->num_ranges; i++)
+            printf("range of %d is: %d", i, msg->ranges[i]);
 //                printf("max disparity of %d is: %d", i, msg->obstacle_set[i].max_disparity);
-        printf("  enabled     = %d\n", msg->status);
-        cout << "point2 " << msg->obstacle_set[0].points[0].y << endl;
+        printf("  enabled     = %d\n", msg->enabled);
+//        cout << "point2 " << msg->obstacle_set[0].points[0].y << endl;
     }
 };
 
